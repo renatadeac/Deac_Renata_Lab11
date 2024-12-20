340589a37b564844e921bfb22910fb3ff82cb7f9
renatadeac <renatadeac33@gmail.com>
2024-12-20 20:13:34 +0200

    Add project files.

---
+++ Deac_Renata_Lab11/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Deac_Renata_Lab11
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Deac_Renata_Lab11/Data/IRestService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Deac_Renata_Lab11.Models;
+
+namespace Deac_Renata_Lab11.Data
+{ 
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+
+        Task DeleteShopListAsync(int id);
+    }
+}
